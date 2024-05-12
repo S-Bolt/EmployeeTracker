425238e1efa9229f858e1bfc014c916aa5ac611e
Sam <sambolton.kw@gmail.com>
2024-05-12 18:17:13 -0400

    seeds.sql work

---
+++ main/db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Finance'),
+       ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 125,000, 1),
+       ('Sales Associate', 75,000, 1),
+       ('Lead Engineer', 200,000, 2),
+       ('Software Engineer', 125,000, 2),
+       ('Account Manager', 145,000. 3),
+       ('Accountant', 100,000. 3),
+       ('Legal Team Lead', 150,000, 4),
+       ('Lawyer', 100,000, 4);
