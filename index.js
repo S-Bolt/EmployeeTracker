5a44c3a0fe4c85e83f72f7ab28a12bea75b501a5
Sam <sambolton.kw@gmail.com>
2024-05-15 20:29:50 -0400

    switch statement bones

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-
+const inquirer = require('inquirer');
 
 // Connect to database
 const pool = new Pool(
@@ -20,4 +20,46 @@ function viewDepartments() {
     }); 
 }
 
-viewDepartments();
+//viewDepartments();
+
+//Creating the inquirer prompts menu
+function employeeManager() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'employeeActions',
+        message: 'What would you like to do?',
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role' , 'View All Departments', 'Add Departments']
+      }
+    ])
+    .then((choice) =>  {
+      switch(choice.employeeActions) {
+        case 'View All Employees':
+        //function;
+           break;
+        case 'Add Employee':
+          //function;
+          break;
+        case 'Update Employee Role':
+            //function;
+            break;
+        case 'View All Roles':
+              //function;
+              break;
+        case 'Add Role':
+            //function;
+            break;
+        case 'View All Departments':
+            //function;
+            break;
+        case 'Add Departments':
+            //function;
+            break;
+      }
+     
+    })
+}
+
+//Call the manager to start
+employeeManager();
